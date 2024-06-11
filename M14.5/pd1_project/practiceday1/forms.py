02177e219249db86eae681c280211d51fc5aaec3
Md Saiyedul Morsalin <mdsaiyedulmorsalin.code@gmail.com>
2024-06-11 06:37:07 +0600

    done ordinary code practice

---
+++ M14.5/pd1_project/practiceday1/forms.py
@@ -0,0 +1,22 @@
+from django import forms 
+import datetime
+class PracticeForm(forms.Form):
+    # Common Fields
+    name = forms.CharField(initial='Your Name')
+    comment = forms.CharField(widget=forms.Textarea)
+    comment1 = forms.CharField(widget=forms.Textarea(attrs={'rows':3}))
+    email = forms.EmailField(required=False,label='Enter Your Email')
+    date = forms.DateField()
+    birth_date = forms.CharField(widget=forms.NumberInput(attrs={'type':'date'}))
+    YEAR_CHOICE = ['2000','2001','2002','2003']
+    birth_day = forms.DateField(widget=forms.SelectDateWidget(years = YEAR_CHOICE))
+    value = forms.DecimalField(initial=10)
+    message = forms.CharField(max_length=20,widget=forms.TextInput(attrs={'placeholder':'Enter your message within 20 characters'}))
+    date_today = forms.DateField(initial=datetime.date.today)
+    agree = forms.BooleanField()
+    # ChoiceField, MultipleChoiceField, ModelChoiceField, and ModelMultipleChoiceField
+    FAVORITE_COLORS = [('white','White'),('black','Black'),('rose','Rose Black')]
+    favorite_color = forms.ChoiceField(choices=FAVORITE_COLORS)
+    select_color = forms.ChoiceField(widget=forms.RadioSelect,choices=FAVORITE_COLORS)
+    multi_color = forms.MultipleChoiceField(choices=FAVORITE_COLORS)
+    
--- M14.5/pd1_project/practiceday1/views.py
+++ M14.5/pd1_project/practiceday1/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from . forms import PracticeForm
 # Create your views here.
+def home(request):
+    if request.method =='POST':
+        form = PracticeForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            
+            return render(request,'home.html',{'form':form})
+    form = PracticeForm()
+    return render(request,'home.html',{'form':form})
